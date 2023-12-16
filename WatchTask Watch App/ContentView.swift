6842b6c5e07a334f75a053aa8c7db915e9ebd2cb
JWSScott777 <jwsscott777@gmail.com>
2023-12-16 12:19:27 -0500

    Added Watch Target

---
+++ WatchTask Watch App/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  WatchTask Watch App
+//
+//  Created by JWSScott777 on 12/16/23.
+//
+
+import SwiftUI
+
+
+struct ContentView: View {
+
+    var body: some View {
+        Text("")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WatchTask Watch App/WatchTaskApp.swift
@@ -0,0 +1,19 @@
+//
+//  WatchTaskApp.swift
+//  WatchTask Watch App
+//
+//  Created by JWSScott777 on 12/16/23.
+//
+
+import SwiftUI
+
+@main
+struct WatchTask_Watch_AppApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                
+        }
+    }
+}
